9da8983365d81668e2596e9d01b91f98cc54ea29
Nate <78760239+NateNoNameSOFT@users.noreply.github.com>
2022-09-04 22:21:54 -0700

    use lib_uno_game, and write name to stream

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,5 @@
-use std::{net::TcpStream, collections::VecDeque, io::stdin};
-use serde_derive::Serialize;
-
-#[derive(Debug)]
-pub struct Game {
-    players: Vec<Player>,
-    deck: VecDeque<Card>,
-    current_card: Card,
-}
-
-#[derive(Debug, Serialize)]
-struct Player {
-    name: String,
-    turn: usize,
-    cards: Vec<Card>,
-}
-
-#[derive(Clone, Debug, Serialize)]
-enum CardKind {
-    WildCard,
-    DrawFour,
-    DrawTwo,
-    Cancel,
-    Reverse,
-    Zero,
-    One,
-    Two,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-}
-
-#[derive(Clone, Debug, Serialize)]
-struct Card {
-    /// Color is None if it is a wildcard, or any card can be placed on it
-    color: Color,
-    kind: CardKind,
-}
-
-#[derive(Clone, Debug, Serialize)]
-enum Color {
-    Red,
-    Blue,
-    Green,
-    Yellow,
-    Wild,
-}
-
-impl Player {
-    fn new() -> Player {
-        Player { name: String::new(), turn: 0, cards: Vec::new() }
-    }   
-    fn set_name(&mut self, name: &str) {
-        self.name = name.into();
-    }
-}
+use std::{net::TcpStream, io::stdin};
+use lib_uno_game::{Player, Game, Packet};
 
 fn main() {
     let stream = TcpStream::connect("127.0.0.1:7878").unwrap();
@@ -65,16 +7,17 @@ fn main() {
     handle_connection(stream);
 }
 
-fn handle_connection(stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream) {
     let mut buf = String::new();
-
     let mut player = Player::new();
 
+    // Get name from user
     stdin().read_line(&mut buf).unwrap();
-    player.set_name(&buf);
+    player.set_name(&buf.trim());
 
     // Send initial data, like name
-    let send = bincode::serialize(&player);
+    let mut packet = Packet::new(&None, &Some(player));
+    packet.write(&mut stream).unwrap();
 
     // Recieve initial cards
 
